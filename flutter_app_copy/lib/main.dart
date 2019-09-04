ba9cd129e244d2c0ac339a3ae31f8396eae9e36d
CHULEI <chulei@birdsh.com>
2019-09-04 17:15:49 +0800

    2019/9/4

--- flutter_app_copy/lib/main.dart
+++ flutter_app_copy/lib/main.dart
@@ -1,50 +1,215 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+      home: RadialExpansionDemo(),
+    ));
 
-class MyApp extends StatelessWidget {
+class Photo extends StatelessWidget {
+  final String photo;
+  final VoidCallback onTap;
+  final double width;
+
+  const Photo({Key key, this.photo, this.onTap, this.width}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Material(
+        color: Theme.of(context).primaryColor.withOpacity(0.25),
+        child: InkWell(
+          onTap: onTap,
+          child: LayoutBuilder(
+            builder: (context, size) {
+              return Image.network(
+                photo,
+                fit: BoxFit.contain,
+              );
+            },
+          ),
+        ));
+  }
+}
+
+class RadialExpansion extends StatelessWidget {
+  final double maxRadius;
+  final clipRectSize;
+  final Widget child;
+
+  const RadialExpansion({Key key, this.maxRadius, this.child})
+      : clipRectSize = 2.0 * (maxRadius / math.sqrt2),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipOval(
+      child: Center(
+        child: SizedBox(
+          width: clipRectSize,
+          height: clipRectSize,
+          child: ClipRect(
+            child: child,
+          ),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class RadialExpansionDemo extends StatelessWidget {
+  static const double kMinRadius = 32.0;
+  static const double kMaxRadius = 128.0;
+  static const opacityCurve =
+      const Interval(0.0, 0.75, curve: Curves.fastOutSlowIn);
 
-  final String title;
+  static RectTween _createRectTween(Rect begin, Rect end) {
+    return MaterialRectCenterArcTween(begin: begin, end: end);
+  }
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(
+  static Widget _buildPage(
+      BuildContext context, String imageName, String description) {
+    return Container(
+      color: Theme.of(context).canvasColor,
+      child: Center(
+        child: Card(
+          elevation: 8,
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              Text(
-                'You have pushed the button this many times:',
+              SizedBox(
+                width: kMaxRadius * 2,
+                height: kMaxRadius * 2,
+                child: Hero(
+                  tag: imageName,
+                  createRectTween: _createRectTween,
+                  child: RadialExpansion(
+                    maxRadius: kMaxRadius,
+                    child: Photo(
+                      photo: imageName,
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ),
+                ),
               ),
               Text(
-                "",
-                style: Theme.of(context).textTheme.display1,
+                description,
+                style: TextStyle(fontWeight: FontWeight.bold),
+                textScaleFactor: 3.0,
               ),
+              const SizedBox(
+                height: 16,
+              )
             ],
           ),
-        ) // This trailing comma makes auto-formatting nicer for build methods.
-        );
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHero(
+      BuildContext context, String imageName, String description) {
+    return Container(
+      width: kMinRadius * 2,
+      height: kMinRadius * 2,
+      child: Hero(
+        createRectTween: _createRectTween,
+        tag: imageName,
+        child: RadialExpansion(
+          maxRadius: kMaxRadius,
+          child: Photo(
+            photo: imageName,
+            onTap: () {
+              Navigator.of(context).push(PageRouteBuilder<void>(pageBuilder:
+                  (BuildContext context, Animation<double> animation,
+                      Animation<double> secondaryAnimation) {
+                return AnimatedBuilder(
+                  animation: animation,
+                  builder: (context, child) {
+                    return Opacity(
+                      opacity: opacityCurve.transform(animation.value),
+                      child: _buildPage(context, imageName, description),
+                    );
+                  },
+                );
+              }));
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    timeDilation = 5.0;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Radial Transition Demo"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        alignment: Alignment.bottomLeft,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            _buildHero(
+                context,
+                "https://raw.githubusercontent.com/flutter/website/master/examples/_animation/hero_animation/images/flippers-alpha.png",
+                "description1"),
+            _buildHero(
+                context,
+                "https://raw.githubusercontent.com/flutter/website/master/examples/_animation/hero_animation/images/flippers-alpha.png",
+                "description2"),
+            _buildHero(
+                context,
+                "https://raw.githubusercontent.com/flutter/website/master/examples/_animation/hero_animation/images/flippers-alpha.png",
+                "description3"),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HeroAnimation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    timeDilation = 10.0;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Basic Hero Animation"),
+      ),
+      body: Center(
+        child: Photo(
+          photo:
+              "https://raw.githubusercontent.com/flutter/website/master/examples/_animation/hero_animation/images/flippers-alpha.png",
+          width: 300,
+          onTap: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute<void>(builder: (context) {
+              return Scaffold(
+                appBar: AppBar(title: Text("Flippers Page")),
+                body: Container(
+                  color: Colors.blue,
+                  padding: EdgeInsets.all(16),
+                  alignment: Alignment.topLeft,
+                  child: Photo(
+                    photo:
+                        "https://raw.githubusercontent.com/flutter/website/master/examples/_animation/hero_animation/images/flippers-alpha.png",
+                    width: 100,
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ),
+              );
+            }));
+          },
+        ),
+      ),
+    );
   }
 }
