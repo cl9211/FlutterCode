54d9c2f1ab52e7fb302d6da7405cb55d4d66f972
CHULEI <chulei@birdsh.com>
2019-09-17 18:13:28 +0800

    2019/9/17

---
+++ xiecheng_flutter_app/lib/dao/travel_tab_dao.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:xiecheng_flutter_app/model/travel_tab_model.dart';
+
+class TravelTabDao {
+  static Future<TravelTabModel> fetch() async {
+    final response = await http
+        .get("http://www.devio.org/io/flutter_app/json/travel_page.json");
+    if(response.statusCode == 200) {
+      Utf8Decoder utf8decoder = Utf8Decoder();
+      var result = json.decode(utf8decoder.convert(response.bodyBytes));
+      return TravelTabModel.fromJson(result);
+    } else {
+      throw Exception('Failed to load travel_page.json');
+    }
+  }
+}
